fbe4f8698107ceec7a49f4b68b898c0366a5b1da
Mann-M <manmohandoley3@gmail.com>
2023-10-10 13:07:56 +0530

    initial entry

---
+++ AddNumbers.py
@@ -0,0 +1,16 @@
+# By predefined values 
+
+# num1 = 20
+# num2 = 30
+
+# print(num1+num2)
+
+
+# by user defined values
+
+num1 = float(input("enter a number: "))
+num2 = float(input("enter a number:"))
+
+sum= num1+num2
+
+print("this is the sum of",num1, "&",num2, "=", sum)
